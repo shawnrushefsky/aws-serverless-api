c92bed9457fdfca8c6b97846ce4f41c375c2e800
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-09-03 13:51:51 -0400

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,9 @@ output "lambda_aliases" {
 }
 
 output "invoke_url" {
-  value = aws_api_gateway_deployment.deployment.invoke_url
+  value = aws_api_gateway_stage.prod.invoke_url
+}
+
+output "api_id" {
+  value = aws_api_gateway_rest_api.gateway.id
 }
