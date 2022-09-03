7981af65f30b9641f35636c4efb5b039bd245c3f
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-09-03 10:17:12 -0400

    fmt

--- main.tf
+++ main.tf
@@ -16,13 +16,13 @@ locals {
   all_methods = {
     for i, v in flatten([
       for path, config in var.api_spec : [
-        for method, arn in config : { 
-          path = path, 
+        for method, arn in config : {
+          path   = path,
           method = upper(method)
-          arn = arn 
+          arn    = arn
         }
       ]
-    ]): i => v
+    ]) : i => v
   }
 }
 
@@ -42,30 +42,30 @@ resource "aws_api_gateway_resource" "paths" {
 resource "aws_api_gateway_method" "methods" {
   for_each = local.all_methods
 
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  resource_id = aws_api_gateway_resource.paths[each.value.path].id
-  http_method = each.value.method
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.paths[each.value.path].id
+  http_method   = each.value.method
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "endpoints" {
   for_each = local.all_methods
 
-  http_method = aws_api_gateway_method.methods[each.key].http_method
-  resource_id = aws_api_gateway_resource.paths[each.value.path].id
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  type = "AWS_PROXY"
+  http_method             = aws_api_gateway_method.methods[each.key].http_method
+  resource_id             = aws_api_gateway_resource.paths[each.value.path].id
+  rest_api_id             = aws_api_gateway_rest_api.gateway.id
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri = each.value.arn
+  uri                     = each.value.arn
 }
 
 resource "aws_lambda_permission" "invoke_from_gateway" {
   for_each = local.all_methods
 
-  statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
   function_name = each.value.arn
-  principal = "apigateway.amazonaws.com"
+  principal     = "apigateway.amazonaws.com"
 
   source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${each.value.method}${each.value.path}"
 }
