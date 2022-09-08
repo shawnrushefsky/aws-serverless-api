4af42e9833517e98314abd6aabe9af992983005a
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-09-08 14:57:27 -0400

    skip that output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "lambda_aliases" {
-  value = { for i, value in local.all_methods : value.function_name => aws_lambda_alias.lambda_aliases[i].arn }
-}
+# output "lambda_aliases" {
+#   value = { for i, value in local.all_methods : value.function_name => aws_lambda_alias.lambda_aliases[i].arn }
+# }
 
 output "invoke_url" {
   value = aws_api_gateway_stage.prod.invoke_url
